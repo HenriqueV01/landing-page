1a316985491cc627c68d467375b8e7be188dc8ae
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-07-02 17:59:46 -0300

    Add: newsletter-form.component

---
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-newsletter-form',
+  standalone: true,
+  imports: [
+    BtnPrimaryComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './newsletter-form.component.html',
+  styleUrl: './newsletter-form.component.scss'
+})
+export class NewsletterFormComponent {
+
+  newsLetterform!: FormGroup;
+
+  constructor(){
+
+    this.newsLetterform = new FormGroup({
+      nome: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+
+  }
+
+  onSubmit(){
+    console.log(this.newsLetterform.value);
+  }
+
+}
